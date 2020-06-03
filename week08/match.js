a1dd0ba267dfae094b9f6b64cc76e6355022e3ea
sj <ze.zh@hotmail.com>
2020-06-03 23:27:58 +0800

    match

--- week08/match.js
+++ week08/match.js
@@ -1,56 +1,122 @@
-
-let selectors = [];
-
 /**
  * selector
- * div div#id.class[attr=val], div
- * 
-
+ * test: div div#id.class[attr=val], div
  */
 function match (selector, element) {
-    selector.split(',').map(p => p.trim()).map(s => parseSelector(s));    // 选择器列表
+    const selectors = selector.split(',').map(p => p.trim()).map(s => parseSelector(s));    // 选择器列表
 }
 
 
-/**
- * 解析一个复杂选择器
- * @param {*} selector 
- * 描述一个复合选择器
- * {
- *  tagName: '',
- *  id: '',
- *  classNames: ['', ''],
- *  attributes: [
- *      attrName: 'href',
- *      attrVal: 'xxx',
- *      relation: '='   // = / ^= / $= / *=
- *  ]
- * }
- */
-function parseSelector(selector) {
+
+// 用状态机解析复杂选择器
+
+// 描述一个复合选择器
+const initSelector = {
+    tag: '',
+    id: [],
+    class: [],
+    attr: {}
+}
+let selectors = []; // 复杂选择器，暂时只考虑子孙关系（即用 space 分隔）
+let selector = Object.assign({}, initSelector);
+
+function parseSelector(s) {
     let state = data;
-    const EOF = Symbol('EOF');
-    for (let c of selector) {
+    for (let c of s) {
+        console.log(c, JSON.stringify(selector));
         state = state(c);
     }
-    state = state(EOF);
+    selectors.push(selector);
+    return selectors;
 }
 
-// 状态机解析复杂选择器
 function data(c) {
     if (c === '#') {
         return idStart;
     } else if (c === '.') {
         return clsStart;
     } else if (c === '[') {
-        return AttrStart;
+        return attrNameStart;
+    } else if (c === ']') {
+        return attrEnd;
+    }else if (c === ' ') {
+        selectors.push(selector);
+        selector = Object.assign({}, initSelector);
+        return data;
     } else {
-        return tagName(c);
+        return tagNameStart(c);
     }
 }
 
-function idStart(c) {
-    if (c === '#') {
-
-    } else 
+function isBackData (c) {
+    return ['#', '.', '[', ' '].indexOf(c) > -1
 }
+
+let id = '';
+function idStart(c) {
+    if (isBackData(c)) {
+        if (id) selector.id.push(id);
+        id = '';
+        return data(c);
+    } else {
+        id += c;
+        return idStart;
+    }
+}
+
+let className = '';
+function clsStart(c) {
+    if (isBackData(c)) {
+        if (className) selector.class.push(className);
+        className = '';
+        return data(c);
+    } else {
+        className += c;
+        return clsStart;
+    }
+}
+
+let attributeName = '',
+    attributeValue = '';
+function attrNameStart(c) {
+    if (isBackData(c)) {
+        if (attributeName) selector.attr[attributeName] = attributeValue || '';
+        attributeName = '';
+        attributeValue = '';
+        return data(c);
+    } else if (c === '=') {
+        return attrValStart;
+    } else if (c === ']') {
+        if (attributeName) selector.attr[attributeName] = attributeValue || '';
+        attributeName = '';
+        attributeValue = '';
+    } {
+        attributeName += c; 
+        return attrNameStart;
+    }
+}
+
+function attrValStart(c) {
+    if (isBackData(c)) {
+        return data(c);
+    } else if (c === ']') {
+        if (attributeName) selector.attr[attributeName] = attributeValue || '';
+        attributeName = '';
+        attributeValue = '';
+        return data;
+    } else {
+        attributeValue += c; 
+        return attrValStart;
+    }
+}
+
+function tagNameStart(c) {
+    if (isBackData(c)) {
+        return data(c);
+    } else {
+        selector.tag += c;
+        return tagNameStart;
+    }
+}
+
+parseSelector('div a#id.class[attr=val]');
